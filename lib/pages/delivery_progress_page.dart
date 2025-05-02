023cbfe5a1331d10a952cc52fae02437be545ec8
bransyah <bransyahtan@gmail.com>
2025-05-02 14:55:20 +0700

    create receipt and delivery progress page

---
+++ lib/pages/delivery_progress_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:food_delivery/components/my_receipt.dart';
+
+class DeliveryProgressPage extends StatelessWidget {
+  const DeliveryProgressPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Delivery In Progress...'),
+        backgroundColor: Colors.transparent,
+      ),
+      body: const Column(children: [MyReceipt()]),
+    );
+  }
+}
